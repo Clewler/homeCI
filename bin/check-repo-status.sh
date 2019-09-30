22911369cc285a7fe515a938f2299aba8c435dd1
Dawid Brylka <dawid.brylka@tieto.com>
2019-09-30 22:19:52 +0200

    [task] copy of jenkins jobs

--- bin/check-repo-status.sh
+++ bin/check-repo-status.sh
@@ -1,13 +1,17 @@
+
+
 PROJ_DIR="/proj/"
-##test comment
+
 remote_command(){
-	sshpass -p $PASSWD ssh -o "StrictHostKeyChecking no" -tt $UNAME@$DEST_IP $@
+	sshpass -p $PASSWD ssh -o 'StrictHostKeyChecking no'  -tt $UNAME@$DEST_IP $@
 }
 
 REF_REPO=`cd ${PROJ_DIR}; ls ${PROJ_DIR} | grep $PROJECT || echo Not Found`
 
 COMMAND="ls ${PROJ_DIR} | grep $PROJECT || echo Not Found"
 PROD_REPO=`remote_command $COMMAND | awk -F" " '{print $1}'`
+
+
 
 PACKET_LOSS=`ping $DEST_IP -c 4 | tail -2 | head -1 | awk -F", " '{print $2}'`
 
@@ -23,11 +27,17 @@ if [ "$PROD_REPO" = "Not" ];then
 	echo "Production repo not found, clonig a new one"
     COMMAND="git clone https://github.com/$USERNAME/$PROJECT.git"
     remote_command $COMMAND
-    echo "Since we deployed newest version now. Exiting the script"
+    echo "SInce we deployed newest version now. Exiting the script"
     cd -
     exit 0
 else
 	echo "Prod repo found"
+
+	COMMAND="cd ${PROJ_DIR}/$PROJECT;echo \`git log --format=format:%H -1\`"
+
+	PRODUCT_SHA=`remote_command $COMMAND | rev | cut -c 2- | rev`
+	echo $PRODUCT_SHA
+
 fi
 
 if [ "$REF_REPO" = "Not Found" ];then
@@ -38,21 +48,19 @@ else
 	cd $PROJ_DIR/$PROJECT
 	echo "Reference repo found"
     git pull -r origin master
-    cd -
+
+    cd ${PROJ_DIR}/$PROJECT
+	REF_SHA=`git log --format=format:%H -1`
+	cd -
 fi
 
-cd ${PROJ_DIR}/$PROJECT
-REF_SHA=`git log --format=format:%H -1`
 
-COMMAND="cd ${PROJ_DIR}/$PROJECT;SHA=`git log --format=format:%H -1`; echo \$SHA; exit"
-PROD_SHA=`remote_command $COMMAND`
-PROD_SHA=`echo $PROD_SHA | rev | cut -c 2- | rev`
 
-if [ "$PROD_SHA" = "$REF_SHA" ];then
+if [ "$PRODUCT_SHA" = "$REF_SHA" ];then
 	echo "Latest version of Repo is deployed"
     exit 0
 else
 	echo "Deploying new version of repo"
-    COMMAND="cd $PROJECT; git pull -r origin master; exit"
+    COMMAND="cd $PROJ_DIR/$PROJECT;echo \`git pull -r origin master\` > /dev/null; exit"
     remote_command $COMMAND
 fi
